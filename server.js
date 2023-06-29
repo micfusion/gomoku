faf8653a9f1011ddffcd851ddcc85c3c003e92d5
micfusion <31150981+micfusion@users.noreply.github.com>
2023-06-29 08:09:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const io = socketIO(server);
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
 let waitingClients = [];
 let matches = [];
 
